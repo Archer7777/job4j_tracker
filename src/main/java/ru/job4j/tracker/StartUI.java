b852033850448c64502f81ef369ed72a7d119879
Alexey <freem4n382@yandex.ru>
2022-05-05 10:36:25 +0300

    7. Date. Отображение даты. [#310017 #292342]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,12 +3,7 @@ package ru.job4j.tracker;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class StartUI extends Item {
-
-    public StartUI(LocalDateTime created) {
-        super(created);
-    }
-
+public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
