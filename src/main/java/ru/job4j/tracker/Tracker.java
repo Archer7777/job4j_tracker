2c1df0d38cdfd36f3e4199225c33af592d20e91e
Admin <Admin@ADMIN413>
2022-08-17 14:57:20 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -24,6 +24,14 @@ public class Tracker {
 
     }
 
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        System.arraycopy(items, index+1, items, index, size-index-1);
+        items[size - 1] = null;
+        size--;
+        return true;
+    }
+
     public Item findById(int id) {
         int index = indexOf(id);
         return index != -1 ? items[index] : null;
